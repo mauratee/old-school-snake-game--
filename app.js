94fe1dfd5fc00fc4d11aacf29e3ed7784910c619
mauratee <mauraethompson@gmail.com>
2021-09-10 22:57:57 +0000

    Change squares to function in js.app, add row to Bootstrap container in index.html

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 "use strict";
 
+// Initialize variables to 0
+let score = 0
+let distanceTravelled = 0
+
+// grab grid div and assign to variable
 const grid = document.querySelector('.grid');
-let squares = Array.from(document.querySelectorAll('.grid div'));
+// Populate the grid div with 340 square divs using template literal in for loop
+const createSquares = () => {
+    for (let i = 1; i <= 340; ++ i) {
+        grid.innerHTML = `${grid.innerHTML} <div class="square" id="square${i}></div>"`
+    }
+}
+
+// Variable to hold the array of game squares
+const squares = document.getElementsByClassName("square");
+console.log(squares)
