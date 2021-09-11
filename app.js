0d78170d9edde26a52c88ec1a8bba3f7fd6c7395
mauratee <mauraethompson@gmail.com>
2021-09-11 01:09:56 +0000

    Fix typo in app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 "use strict";
 
+
+// || Game Board Construction ||
+
 // Initialize variables to 0
 let score = 0
 let distanceTravelled = 0
@@ -9,10 +12,14 @@ const grid = document.querySelector('.grid');
 // Populate the grid div with 340 square divs using template literal in for loop
 const createSquares = () => {
     for (let i = 1; i <= 340; ++ i) {
-        grid.innerHTML = `${grid.innerHTML} <div class="square" id="square${i}></div>"`
+        grid.innerHTML = `${grid.innerHTML} <div class="square" id="square${i}"></div>`;
     }
 }
 
 // Variable to hold the array of game squares
 const squares = document.getElementsByClassName("square");
 console.log(squares)
+
+
+// || Game Board Logic ||
+createSquares();
