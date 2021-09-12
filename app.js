9d20c8bff0839e623d600e4413dee7c005e2a3f6
mauratee <mauraethompson@gmail.com>
2021-09-12 02:17:46 +0000

    Finish de-bugging grid

--- app.js
+++ app.js
@@ -7,11 +7,14 @@
 let score = 0
 let distanceTravelled = 0
 
+// const GAME_PIXEL_COUNT = 40;
+// const SQUARE_OF_GAME_PIXEL_COUNT = Math.pow(GAME_PIXEL_COUNT, 2);
+
 // grab grid div and assign to variable
 const grid = document.querySelector('.grid');
 // Populate the grid div with 340 square divs using template literal in for loop
 const createSquares = () => {
-    for (let i = 1; i <= 340; ++ i) {
+    for (let i = 1; i <= 1200; ++ i) {
         grid.innerHTML = `${grid.innerHTML} <div class="square" id="square${i}"></div>`;
     }
 }
